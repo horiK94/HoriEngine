3ce273f3b562d0910b0d00f8510f5ae29c2e13a8
hamity <hazemi94basic@gmail.com>
2024-05-26 00:50:05 +0900

    文字列変換処理のひな形追加

--- HoriEngine/Main.cpp
+++ HoriEngine/Main.cpp
@@ -1,12 +1,26 @@
 #include <Windows.h>
 #include <iostream>
 
+namespace HoriEngine::String
+{
+	std::wstring ToUtf16(const std::u32string& str)
+	{
+		//UTF-32 -> UTF-16
+		return L"";
+	}
+
+	std::string ToUtf8(const std::u32string& str)
+	{
+		//UTF-32 -> UTF-16
+		return "";
+	}
+}
+
 namespace HoriEngine::Debug
 {
 	/// @brief コンソールを開く
 	void OpenConsole()
 	{
-		//::OutputDebugStringW(L"Hello, World\n");
 		::AllocConsole();
 
 		FILE* file = nullptr;
@@ -15,6 +29,27 @@ namespace HoriEngine::Debug
 		::freopen_s(&file, "CONOUT$", "w", stderr);
 
 		//::Sleep(10000);
+	}
+
+	void CloseConsole()
+	{
+		getchar();
+		::FreeConsole();		//Allocしたものを閉じる
+	}
+
+	//void OutputDebug(std::string str)		//ShiftJIS　-> UTF-8(設定で変更)
+	//{
+	//	//OutputDebugStringAはOSのデフォルト文字エンコーディングが設定される
+	//	::OutputDebugStringA("Hello World");		//マルチバイト文字列. (環境によるが、日本語だとデフォルト)SHIFT-JIS
+	//	::OutputDebugStringW(L"Hello World");		//ワイド文字列(UTF-16)
+	//	u8"Hello World";	//UTF-8
+	//	u"Hello World";		//UTF-16
+	//	U"Hello World";		//UTF-32
+	//}
+
+	void OutputDebug(const std::u32string& str)
+	{
+		::OutputDebugStringW(String::ToUtf16(str).c_str());
 	}
 }
 
@@ -28,8 +63,14 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR lpCmdLine, int nCmdShow
 	//呼び出し側の変え方 
 	std::cout << nCmdShow << '\n';
 
-	getchar();
-	::FreeConsole();		//Allocしたものを閉じる
+	std::cout << sizeof("あい") << '\n';		//2byte * 2 + NULL文字1byte = 5byte
+	std::cout << sizeof(u8"あい") << '\n';		//日本語3byte * 2 + NULL文字1byte = 7byte
+	std::cout << sizeof(u"あい") << '\n';		//日本語3byte * 2 + NULL文字1byte = 7byte
+	std::cout << sizeof(U"あい") << '\n';		//日本語3byte * 2 + NULL文字1byte = 7byte
+
+	HoriEngine::Debug::OutputDebug(U"Hello, World");
+
+	HoriEngine::Debug::CloseConsole();
 
 	return 0;
 }
